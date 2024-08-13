8915ad97511481c7f4849a2076aa9b9b616b808f
Wender Mata <wender.mata@live.com>
2024-08-12 22:52:52 -0300

    [FEAT]: ajustes csproj

--- src/WebApi/Program.cs
+++ src/WebApi/Program.cs
@@ -12,12 +12,8 @@ builder.Services.AddConfigureControllers()
     .AddSwaggerGen();
 
 var app = builder.Build();
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
